ed346c3e3ee6e10ea310b9c6c00a5be6d50c4ccb
Adam Williams <adam.williams@lifeomic.com>
2019-01-22 10:18:53 -0500

    Simplify invocation event creation

--- execute.ts
+++ execute.ts
@@ -1,19 +1,21 @@
 import {
+  createLocalInvocationEvent,
   executeSingleHandlerLocal
 } from '@jupiterone/jupiter-managed-integration-sdk';
-
 import { createLogger, TRACE } from 'bunyan';
-
 import executionHandler from './index';
 
 async function run(): Promise<void> {
   const logger = createLogger({ name: 'local', level: TRACE });
+  const integrationConfig = {};
+
   logger.info(
-    await executeSingleHandlerLocal({}, logger, executionHandler, {
-      accountId: '',
-      instanceCore: {},
-      timestamp: Date.now()
-    }),
+    await executeSingleHandlerLocal(
+      integrationConfig,
+      logger,
+      executionHandler,
+      createLocalInvocationEvent()
+    ),
     'Execution completed successfully!'
   );
 }
