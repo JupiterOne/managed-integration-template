2670c60fdbf295f4d0e4dfc217f7aae6f53eec69
Adam Williams <adam.williams@lifeomic.com>
2019-03-19 14:27:10 -0400

    Fix moduleFileExtensions to include defaults

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
+const { defaults } = require("jest-config");
+
 module.exports = {
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.ts?$": "ts-jest",
   },
   testMatch: ["<rootDir>/src/**/*.test.{js,ts}"],
   collectCoverageFrom: [
@@ -8,7 +10,7 @@ module.exports = {
     "!src/index.ts",
     "!src/ProviderClient.ts",
   ],
-  moduleFileExtensions: ["ts", "js"],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "ts"],
   testEnvironment: "node",
   clearMocks: true,
   collectCoverage: true,
